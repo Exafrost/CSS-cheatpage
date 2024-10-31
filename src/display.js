78d4afb1cce105871701cb4767473e38ebeb9049
Exafrost <auresacha@gmail.com>
2024-10-31 12:03:10 +0100

    [feat] Core of the page + beginning of buttons

---
+++ src/display.js
@@ -0,0 +1,3 @@
+function display(ref){
+    window.location.assign("/src/display.html")
+}
---
+++ src/tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+
+// npx tailwindcss -i ./input.css -o ./output.css --watch
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily :{
+        lucon: ['lucon', 'sans-serif'],
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        wiggleHard : {
+          '0%, 100%': { transform: 'rotate(-15deg)' },
+          '50%': { transform: 'rotate(15deg)' },
+        },
+        wiggleVeryHard : {
+          '0%, 100%': { transform: 'rotate(-60deg)' },
+          '50%': { transform: 'rotate(60deg)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        wiggleHard: 'wiggleHard 1s ease-in-out infinite',
+        wiggleVeryHard: 'wiggleVeryHard 1s ease-in-out infinite',
+    }
+  },
+  },
+  plugins: [],
+}
+
